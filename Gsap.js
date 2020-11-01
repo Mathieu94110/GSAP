a291d7f87c2132937ea6440e7002fd158c3dfe6a
Mathieu Enault <me94110@gmail.com>
2020-11-01 22:17:08 +0100

    adding ghost interaction and arrival of emoticon

--- Gsap.js
+++ Gsap.js
@@ -1,7 +1,3 @@
-/*const logo = document.getElementById("logo") ;
-gsap.to(logo, 2,{width:200 , height:200});
-*/
-
 const conveyor = document.getElementById("conveyor");
 
 const arm = document.getElementById("arm");
@@ -10,18 +6,18 @@ const emoticon = document.getElementById("emoticon");
 
 function start(eyes, mouth) {
   console.log(eyes);
-  let div = document.getElementById("div");
-  let imgEyes = document.createElement("img");
-  imgEyes.src = eyes + ".png";
-  imgEyes.setAttribute("id", "img1");
-  imgEyes.style.position = "absolute";
-  div.append(imgEyes);
+  let img1 = document.getElementById("img1");
+  img1.src = eyes + ".png";
+  img1.style.visibility = "visible"; //
 
   let imgMouth = document.createElement("img");
   imgMouth.src = mouth + ".png";
   imgMouth.setAttribute("id", "img2");
   imgMouth.style.position = "absolute";
   div.append(imgMouth);
+
+  //saut de t'émoticon
+  let R = 200;
 
   //saut de Bowser
 
@@ -78,7 +74,23 @@ function start(eyes, mouth) {
       rotation: 2,
     });
 
-  //
+  //Mouvement du fantome
+  TweenMax.to("#gost", 6, {
+    bezier: {
+      curviness: 1.5,
+      values: [
+        { x: 0, y: 0 },
+        { x: R / 2, y: R / 2 },
+        { x: 0, y: R },
+        { x: -R / 2, y: R / 2 },
+        { x: 0, y: 0 },
+      ],
+    },
+
+    rotation: 720,
+    ease: Power0.easeNone,
+    repeat: -1,
+  });
 
   //
 
@@ -109,7 +121,7 @@ function start(eyes, mouth) {
   ////////////////////////////////////////
   gsap.to(img1, {
     duration: 2.5,
-    x: 45,
+    x: 55, ////
     delay: 9,
     ease: "elastic.out(1, 0.3)",
   }); // Il se prend le champigon et recule
@@ -117,14 +129,14 @@ function start(eyes, mouth) {
   gsap.to(img1, {
     ////oeil prend son elan
     duration: 0.5,
-    x: -90,
+    x: -95,
     delay: 10,
   });
 
   gsap.to(img1, {
     /////oeil mais coup de tete
     duration: 0.3,
-    x: 95,
+    x: 105,
     delay: 10.5,
   });
 
@@ -145,4 +157,63 @@ function start(eyes, mouth) {
     { x: 0, y: 0 },
     { rotation: 260, x: 1300, y: -50, scale: 1.5, duration: 5, delay: 10.6 }
   ); // Le champigonon s'éjecte
+
+  ///////////////////////////////////////////////
+  gsap.to(emoticon, {
+    ////// tete s'approche du mur
+    duration: 1,
+    x: 750,
+    delay: 11.8,
+  });
+  gsap.to(img1, {
+    /////oeil ...
+    duration: 1,
+    x: 180,
+    delay: 11.8,
+  });
+  gsap.to(emoticon, {
+    ////// tete saute
+    duration: 2,
+    delay: 15.8,
+    x: 950,
+  });
+
+  TweenMax.set("img1", { xPercent: "-50%", yPercent: "-50%" });
+  tl = new TimelineMax();
+  tl.to("#img1", 2, { rotation: 360, x: 380, delay: 15.8 });
+
+  gsap.to(emoticon, {
+    duration: 2,
+    delay: 17.8,
+    ease: "slow(0.7, 0.7, false)",
+    x: 1020,
+  });
+  gsap.to(img1, {
+    duration: 2,
+    delay: 17.8,
+    ease: "slow(0.7, 0.7, false)",
+    x: 451,
+  });
+  gsap.to(img2, {
+    duration: 2,
+    delay: 19.8,
+    y: 355,
+  });
+  gsap.to(img1, {
+    duration: 2,
+    delay: 21.8,
+    ease: "slow(0.7, 0.7, false)",
+    x: 470,
+  });
+  gsap.to(img2, {
+    duration: 2,
+    delay: 21.8,
+    x: 20,
+  });
+  gsap.to(emoticon, {
+    duration: 2,
+    delay: 21.8,
+    ease: "slow(0.7, 0.7, false)",
+    x: 1040,
+  });
 }
